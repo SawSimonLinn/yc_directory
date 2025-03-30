4b9a03234e1967b1f7d864493b3f7de042959154
Simon <sawsimonelinn@gmail.com>
2025-03-29 21:41:04 -0700

    update app/(root)/layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import { ReactNode } from 'react';
 
-export default function Layout ({children } : Readonly<{children: ReactNode}>) {
+export default function Layout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
-    <main className="font-work-sans">
+    <main className='font-work-sans'>
       <Navbar />
       {children}
       <Footer />
     </main>
-  )
+  );
+}
